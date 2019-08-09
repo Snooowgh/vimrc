dbacffbed8db80b3fd583366506743f59b7a8494
Sen Wang <sustechapp@qq.com>
2019-08-09 21:55:00 +0800

    Update install_awesome_vimrc.sh

--- install_awesome_vimrc.sh
+++ install_awesome_vimrc.sh
@@ -13,6 +13,20 @@ source ~/.vim_runtime/vimrcs/extended.vim
 try
 source ~/.vim_runtime/my_configs.vim
 catch
-endtry' > ~/.vimrc
+endtry
+set number
+nnoremap j gj
+nnoremap k gk
+nnoremap E $
+nnoremap B ^
+nnoremap gV `[v`]
+inoremap jk <esc>
+nnoremap ii :w<CR>
+nnoremap II :wq<CR>
+nnoremap <leader>s :mksession!<CR>
+nnoremap <leader>m :!make<CR>
+nnoremap <leader>p :w<CR>:!python %<CR>
+set autoindent
+' > ~/.vimrc
 
 echo "Installed the Ultimate Vim configuration successfully! Enjoy :-)"
